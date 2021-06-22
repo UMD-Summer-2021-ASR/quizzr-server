ac06dad822696bfdb82db180202eacc3be9fc8dc
Christopher Rapp <christopher.michael.rapp@gmail.com>
2021-06-22 13:14:24 -0400

    Added MongoDB interactions

--- README.md
+++ README.md
@@ -1,1 +1,31 @@
-Readme stub
+# Description
+This is the code for the Quizzr server written in the Flask framework for Python 3.8. It handles POST requests to the
+`/upload/` webpage that include a form with the MIME type "multipart/form-data". The form must include the string fields
+`questionId` and `userId` and a file under the field `audio`. Currently, it only updates the MongoDB Quizzr Atlas in
+response to these requests and does not store the upload at all.
+# Installation
+1. Clone this repository.
+2. Install `pip`.
+3. Install all the necessary dependencies by executing `pip install requirements.txt` in the folder of the repository.
+4. Get the connection string from the MongoDB Quizzr Atlas and copy it into a text file.
+5. Set the environment variable `CONNECTION_STRING_PATH` to be the absolute path of the text file on your machine by
+   executing `export CONNECTION_STRING_PATH=<path>`, replacing `<path>` with this path.
+# Running the Server for Testing
+The following instructions are for running the server for testing purposes. Please do not follow these instructions if
+you plan on running it in production.
+
+To start the server, enter the following commands into the terminal:
+```bash
+$ export FLASK_APP=server
+$ flask run
+```
+You can view the website through http://127.0.0.1:5000/. \
+Stop the server using Ctrl + C.
+
+There is an option for running this server in debug mode. To do that, simply set `FLASK_ENV` to `development` in the
+terminal. By default, the debugger is enabled. To disable the debugger, add `--no-debugger` to the run command.
+## Testing the Upload Handler
+To test the upload handler component of the server, navigate to the page `/uploadtest/` and fill in the fields in the
+resulting GUI. You will need to refer to the MongoDB Quizzr Atlas to fill in the Question ID and User ID fields. Note
+that upon submitting, the contents of the atlas will be altered, and the server has no built-in way of reversing these 
+changes.
--- server.py
+++ server.py
@@ -1,13 +1,23 @@
 # from typing import Dict, List, Union
+from os import environ
 
-from flask import Flask, request
-# from flask import request
+from bson import ObjectId
+from flask import Flask, request, render_template
+import pymongo
 
 app = Flask(__name__)
 
-# TEMPORARY.
 version = "0.0.0"
-users = {"johnDoe": {"recordedAudios": []}}
+with open(environ['CONNECTION_STRING_PATH']) as f:
+    client = pymongo.MongoClient(f.read())
+
+database = client.QuizzrDatabase
+users = database.Users
+rec_questions = database.RecordedQuestions
+unrec_questions = database.UnrecordedQuestions
+audio_descriptors = database.Audio
+
+"""users = {"johnDoe": {"recordedAudios": []}}
 rec_questions = {
     "question1": {
         "transcript": "The quick brown fox jumps over the lazy dog.", "recordings": ["audio1"]
@@ -28,46 +38,62 @@ audio_descriptors = {
         "user": "johnDoe",
         "question": "question1"
     }
-}
+}"""
+
 
 @app.route("/")
 def hello_world():
-    return "<p>Hello, World!</p>"
+    return "<p>This is the home page of the website.</p>"
 
 
 @app.route("/upload/", methods=["POST"])
 def recording_listener():
     if request.method == "POST":
-        recording = request.form["audio"]
-        question_id = request.form["questionId"]
-        user_id = request.form["userId"]
+        recording = request.files["audio"]
+        # question_id = request.form["questionId"]
+        # user_id = request.form["userId"]
+        question_id = ObjectId(request.form["questionId"])
+        user_id = ObjectId(request.form["userId"])
 
-        try:
-            transcript = unrec_questions[question_id]
-            unrecorded = True
-        except KeyError:
-            transcript = rec_questions[question_id]
+        question_query = {"_id": question_id}
+        unrecorded = True
+        question = unrec_questions.find_one_and_delete(question_query)
+        if question is None:
             unrecorded = False
+            question = rec_questions.find_one(question_query)
 
+        transcript = question["transcript"]
         file_id = upload_file(recording)
         vtt = get_vtt(recording, transcript)
 
-        audio_id = generate_audio_id()
-        audio_entry = {"fileId": file_id, "vtt": vtt, "version": version, "user": user_id, "question": question_id,
+        audio_id = file_id
+        audio_entry = {"_id": file_id, "vtt": vtt, "version": version, "user": user_id, "question": question_id,
                        "accuracy": get_accuracy(transcript, recording)}
-        # TODO: Put the set of instructions below in MongoDB, appropriately translated.
-        audio_descriptors[audio_id] = audio_entry
+
+        audio_descriptors.insert_one(audio_entry)
+
         if unrecorded:
-            temp_question = unrec_questions.pop(question_id, None)
-            temp_question["recordings"] = [audio_id]
-            unrec_questions[question_id] = {}
-            rec_questions[question_id] = temp_question
+            question["recordings"] = [audio_id]
+            rec_questions.insert_one(question)
         else:
-            rec_questions[question_id]["recordings"].append(audio_id)
-        users[user_id]["recordedAudios"].append(audio_id)
+            rec_questions.update_one(question_query, {"$push": {"recordings": audio_id}})
+
+        user_query = {"_id": user_id}
+        user = users.find_one(user_query)
+        user["recordedAudios"].append(audio_id)
+        users.update_one(user_query, {"$push": {"recordedAudios": audio_id}})
+
+        return render_template("submission.html")
+
+    return "<p>Hello, World!</p>"
 
 
+@app.route("/uploadtest/")  # Do not include in deployment.
 def recording_listener_test():
+    return render_template("uploadtest.html")
+
+
+def recording_listener_test_desktop():
     print("Users: ", users)
     print("Recorded Questions: ", rec_questions)
     print("Unrecorded Questions: ", unrec_questions)
@@ -110,7 +136,7 @@ def recording_listener_test():
 
 
 def upload_file(file):
-    return "placeholder file ID 2"
+    return ObjectId()
 
 
 def get_vtt(audio, transcript):
@@ -126,4 +152,5 @@ def get_accuracy(transcript, recording):
 
 
 if __name__ == "__main__":
-    recording_listener_test()
+    # recording_listener_test_desktop()
+    pass
