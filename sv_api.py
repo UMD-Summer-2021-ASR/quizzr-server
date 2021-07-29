0dc9c0124901fbb931d7adfbf640715b62126e46
Christopher Rapp <christopher.michael.rapp@gmail.com>
2021-07-29 13:35:59 -0400

    Move one-line function explanations; remove unnecessary _verify_id_token() calls; add /profile and /profile/<username> endpoints; prepare for access policy control

---
+++ sv_api.py
@@ -0,0 +1,8 @@
+# Include several utility methods such as getting a route by operation ID.
+class QuizzrAPISpec:
+    def __init__(self):
+        pass
+
+    def path_for(self, op_id):
+        pass
+
